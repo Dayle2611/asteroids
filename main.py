5a65c80581c162e93b049275deade6004b7de583
Dayle Watson <dwatson1191@gmail.com>
2026-05-05 17:56:18 +0100

    added constants.py file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import pygame
+from constants import *
+# from logger import log_state
+
 def main():
-    print("Hello from asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 if __name__ == "__main__":
